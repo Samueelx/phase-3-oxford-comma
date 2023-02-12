aa210180caf3e5c150efb5cd267e7b4294e62c37
Samueelx <gsammygitau@gmail.com>
2023-02-12 09:52:36 +0300

    done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    return_string = ""
+    if array.length == 1
+        return_string = array.join();
+    elsif array.length == 2
+        return_string = array.join(" and ")
+    else 
+        array.each.with_index do |str, index|
+            if index != array.length-1
+                return_string << "#{str}, "
+            else return_string << "and #{str}"
+            end
+        end
+    end
 
+    return_string
 end
